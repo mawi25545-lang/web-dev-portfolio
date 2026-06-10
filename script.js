d566f508b9f6ae77ab3ad1053dbe0a933291a4ff
mawi25545-lang <mawi25545@mpsedu.org>
2026-06-09 20:26:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const projects = document.querySelectorAll ('project');
 
+
+
+projects.forEach(project => {
+  project.addEventListener('mouseenter', () => {
+    if (window.innerWidth > 600){
+      project.style.transform = 'scale(1.1)';
+    project.style.transition = 'transform 0.3s ease';
+    }
+  });
+    project.addEventListener('mouseleave', () => {
+    if (window.innerWidth > 600){
+      project.style.transform = 'scale(1)';
+    project.style.transition = 'transform 0.3s ease';
+    }
+  });
+});
